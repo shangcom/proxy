aeebcc2812963b55852288a6b1cacb50ddabe9e3
shangcom <dndbs91@gmail.com>
2024-10-26 20:24:21 +0900

    스프링 핵심 원리 - 고급편
    섹션5 - 프록시 패턴과 데코레이터 패턴
    
    예제 프로젝트 만들기 v2
    
    완료

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,18 +1,19 @@
 package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class) // 설정 클래스 지정
 /*
  config 바꿔가면서 할건데, 스프링 부트는 기본이 전체 스캔함.
  그래서 app 패키지로 스캔 범위 한정하고, config는 스캔 못하게 함.
  (@Configuration은 스캔 대상이라서, config 폴더에 들어있는 모든 파일 스캔됨.)
  대신 수동으로 설정 클래스 지정하기 위해 Import(xxx.class) 사용.
  */
-@SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
+@Import({AppV1Config.class, AppV2Config.class}) // 설정 클래스 지정. {}로 여러개 가능.
+@SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -0,0 +1,29 @@
+package hello.proxy.app.v2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@RequestMapping// @ComponentScan 대상 아님. 수동등록 할거라서 @Controller 붙이면 안됨.
+@ResponseBody
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+
+    public OrderControllerV2(OrderServiceV2 orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v2/no-log")
+    public String noLog() {
+        return "ok";
+    }
+}
