46823d6662f2322c2573a89d15e35ff7d60f9fe1
shangcom <dndbs91@gmail.com>
2024-10-27 11:54:35 +0900

    스프링 핵심 원리 - 고급편
    섹션5 - 프록시 패턴과 데코레이터 패턴
    
    프록시 패턴 - 예제 코드1
    
    완료

---
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,25 @@
+package hello.proxy.pureproxy.proxy;
+
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ProxyPatternTest {
+
+    /*
+     캐시 기능이 없는 경우.
+     같은 대상에 대한 조회가 각각 1초씩 3초 걸림.
+     한번 조회했던 데이터는 캐시로 보관하여 바로 반환 필요성.
+     */
+    @Test
+    void noProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+}
