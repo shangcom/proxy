967b0fc072e8e59d7eb6404006bc19311c22b126
shangcom <dndbs91@gmail.com>
2024-10-27 16:28:22 +0900

    스프링 핵심 원리 - 고급편
    섹션5 - 프록시 패턴과 데코레이터 패턴
    
    인터페이스 기반 프록시 - 적용
    
    완료

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,11 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV1Config;
-import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.trace.logtrace.LogTrace;
+import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 /*
@@ -12,12 +14,17 @@ import org.springframework.context.annotation.Import;
  (@Configuration은 스캔 대상이라서, config 폴더에 들어있는 모든 파일 스캔됨.)
  대신 수동으로 설정 클래스 지정하기 위해 Import(xxx.class) 사용.
  */
-@Import({AppV1Config.class, AppV2Config.class}) // 설정 클래스 지정. {}로 여러개 가능.
+//@Import({AppV1Config.class, AppV2Config.class}) // 설정 클래스 지정. {...}로 여러개 가능.
+@Import(InterfaceProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProxyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyApplication.class, args);
+    }
 
+    @Bean
+    public LogTrace logTrace() {
+        return new ThreadLocalLogTrace();
+    }
 }
