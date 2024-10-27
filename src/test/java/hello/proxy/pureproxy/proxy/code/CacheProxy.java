5d48c1285e5e676e57e4796801e888c0ed38f4c9
shangcom <dndbs91@gmail.com>
2024-10-27 12:40:39 +0900

    스프링 핵심 원리 - 고급편
    섹션5 - 프록시 패턴과 데코레이터 패턴
    
    데코레이터 패턴 - 예제 코드1
    
    완료

--- src/test/java/hello/proxy/pureproxy/proxy/code/CacheProxy.java
+++ src/test/java/hello/proxy/pureproxy/proxy/code/CacheProxy.java
@@ -2,6 +2,11 @@ package hello.proxy.pureproxy.proxy.code;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * 기존 객체의 operation() 메서드에 캐싱이라는 추가 작업 수행
+ * -> 프록시 패턴.
+ * 아예 새롱ㄴ
+ */
 @Slf4j
 public class CacheProxy implements Subject {
 
