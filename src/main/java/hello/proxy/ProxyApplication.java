26a5c8c5f49923a11a52d0b74b07d02ff0f20d7b
shangcom <dndbs91@gmail.com>
2024-10-29 16:46:04 +0900

    스프링 핵심 원리 - 고급편
    섹션6 - 동적 프록시 기술
    
    JDK 동적 프록시 - 적용2
    
    완료

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,6 +1,6 @@
 package hello.proxy;
 
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -17,7 +17,8 @@ import org.springframework.context.annotation.Import;
 //@Import({AppV1Config.class, AppV2Config.class}) // 설정 클래스 지정. {...}로 여러개 가능.
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
-@Import(DynamicProxyBasicConfig.class)
+//@Import(DynamicProxyBasicConfig.class)
+@Import(DynamicProxyFilterConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
 public class ProxyApplication {
 
---
+++ src/main/java/hello/proxy/config/v2_dynamicproxy/handler/LogTraceFilterHandler.java
@@ -0,0 +1,54 @@
+package hello.proxy.config.v2_dynamicproxy.handler;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.util.PatternMatchUtils;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * 메서드 이름 필터.
+ */
+public class LogTraceFilterHandler implements InvocationHandler {
+    private final Object target;
+    private final LogTrace logTrace;
+    private final String[] patterns;
+
+    public LogTraceFilterHandler(Object target, LogTrace logTrace, String...
+            patterns) {
+        this.target = target;
+        this.logTrace = logTrace;
+        this.patterns = patterns;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws
+            Throwable {
+
+        // 메서드 이름 필터.
+        // 조건에 해당하지 않을 경우, 로그를 남기지 않는다(no-log).
+        String methodName = method.getName();
+        if (!PatternMatchUtils.simpleMatch(patterns, methodName)) {
+
+            return method.invoke(target, args);
+        }
+
+        TraceStatus status = null;
+        try {
+            String message = method.getDeclaringClass().getSimpleName() + "."
+                    + method.getName() + "()";
+            status = logTrace.begin(message);
+
+            //로직 호출
+            Object result = method.invoke(target, args);
+
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+
+    }
+}
